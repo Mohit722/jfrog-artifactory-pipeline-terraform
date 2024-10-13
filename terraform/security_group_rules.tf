c11ef006def56bce59ec0ec321e6fc2b02686ca2
Mohit722 <91499522+Mohit722@users.noreply.github.com>
2024-10-14 01:29:52 +0530

    Update security_group_rules.tf

--- terraform/security_group_rules.tf
+++ terraform/security_group_rules.tf
@@ -1,13 +1,14 @@
-# Security group rule for JFrog Artifactory
 
+# Security group rule for JFrog Artifactory
 resource "aws_security_group_rule" "artifactory_ingress" {
+  count             = 2  # Create two rules
   type              = "ingress"
-  from_port         = 8082
-  to_port           = 8082
+  from_port         = [8081, 8082][count.index]  # Set the port based on count index
+  to_port           = [8081, 8082][count.index]
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "sg-0a4b86efefd9999b7"  # Replace with your security group ID
 
   # Optional description
-  description = "Allow inbound traffic on port 8082 for JFrog Artifactory"
+  description = "Allow inbound traffic on port ${[8081, 8082][count.index]} for JFrog Artifactory"
 }
